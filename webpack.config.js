481272b7b176a41e0c3d5f5f921424779acee9d8
Osvaldo Costa <osvaldo.costajr@gmail.com>
2021-02-23 21:03:25 -0300

    S18C271 - Configuraçao do Webpack #2

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  // enquanto em dev
+  mode: 'development',
+  entry: './src/A0018-webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  // marcar no ts o item debugado no navegador
+  devtool: 'source-map',
+};
