60da50d9df1aae5fe9405c231063b51ac57ab5ac
Osvaldo Costa <osvaldo.costajr@gmail.com>
2022-08-27 13:45:56 -0300

    S20C304 - Dependency Inversion

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 module.exports = {
   // enquanto em dev
   mode: 'development',
-  entry: './src/A0027-static/A0027-static.ts',
+  entry: './src/A0030-associacao/A0030-associacao.ts',
   module: {
     rules: [
       {
